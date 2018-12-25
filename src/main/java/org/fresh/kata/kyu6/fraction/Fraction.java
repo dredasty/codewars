8b8230091ac97fb185b0fcd01b3e60d38f70a0f1
Oleksii Pasko <uncommon.fresh@gmail.com>
2018-12-25 19:00:34 +0200

    adding draft

---
+++ src/main/java/org/fresh/kata/kyu6/fraction/Fraction.java
@@ -0,0 +1,37 @@
+package org.fresh.kata.kyu6.fraction;
+
+public class Fraction implements Comparable<Fraction> {
+
+    private final long top;
+
+    private final long bottom;
+
+    public Fraction(long numerator, long denominator) {
+        top = numerator;
+        bottom = denominator;
+    }
+
+    @Override
+    public int hashCode() {
+        return 17 * Long.hashCode(top) + Long.hashCode(bottom);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return compareTo((Fraction) o) == 0;
+    }
+
+    @Override
+    public int compareTo(Fraction f2) {
+        return Long.compare(top * f2.bottom, f2.top * bottom);
+    }
+
+    // Your work here!
+
+    //Add two fractions
+    public Fraction add(Fraction f2) {
+        return null;
+    }
+
+    //...and make this class string representable
+}
