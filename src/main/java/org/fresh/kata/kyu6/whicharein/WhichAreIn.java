a31b150b9f37cf7f19a5ebab0a68c1830b2c6b26
Oleksii Pasko <uncommon.fresh@gmail.com>
2018-12-27 17:01:43 +0200

    add draft

---
+++ src/main/java/org/fresh/kata/kyu6/whicharein/WhichAreIn.java
@@ -0,0 +1,7 @@
+package org.fresh.kata.kyu6.whicharein;
+public class WhichAreIn {
+
+    public static String[] inArray(String[] array1, String[] array2) {
+        return new String[] { "arp" };
+    }
+}
