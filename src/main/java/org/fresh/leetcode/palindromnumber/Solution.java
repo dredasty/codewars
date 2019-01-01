25e7e8df4e424eb82dc0af3d932bc1165313a3aa
Oleksii Pasko <uncommon.fresh@gmail.com>
2019-01-01 20:08:15 +0200

    solved https://leetcode.com/problems/palindrome-number/

--- src/main/java/org/fresh/leetcode/palindromnumber/Solution.java
+++ src/main/java/org/fresh/leetcode/palindromnumber/Solution.java
@@ -1,5 +1,11 @@
 package org.fresh.leetcode.palindromnumber;
 
+/**
+ * https://leetcode.com/problems/palindrome-number/
+ */
 public class Solution {
-
+    public boolean isPalindrome(int x) {
+        String str = Integer.valueOf(x).toString();
+        return str.equals(new StringBuilder(str).reverse().toString());
+    }
 }
--- src/test/java/org/fresh/leetcode/palindromenumber/SolutionTest.java
+++ src/test/java/org/fresh/leetcode/palindromenumber/SolutionTest.java
@@ -1,5 +1,24 @@
 package org.fresh.leetcode.palindromenumber;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.fresh.leetcode.palindromnumber.Solution;
+import org.junit.jupiter.api.Test;
+
 public class SolutionTest {
 
+    @Test
+    public void testSolution() {
+        Solution solution = new Solution();
+        assertTrue(solution.isPalindrome(121));
+        assertFalse(solution.isPalindrome(122));
+        assertFalse(solution.isPalindrome(-123));
+        assertFalse(solution.isPalindrome(-1));
+        assertTrue(solution.isPalindrome(0));
+        assertFalse(solution.isPalindrome(21));
+        assertFalse(solution.isPalindrome(2147483647));
+        assertTrue(solution.isPalindrome(123321));
+        assertTrue(solution.isPalindrome(1234321));
+    }
 }
