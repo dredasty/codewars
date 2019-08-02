bd6898d2fc34776a9dd46d2e81dc26c8c00160ac
Oleksii Pasko <uncommon.fresh@gmail.com>
2019-08-02 10:56:30 +0300

    solved

---
+++ src/main/java/org/fresh/leetcode/easy/containsduplicate_217/Solution.java
@@ -0,0 +1,16 @@
+package org.fresh.leetcode.easy.containsduplicate_217;
+
+import java.util.HashSet;
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet set = new HashSet<>(nums.length);
+        for (int number : nums) {
+            if (set.contains(number)) {
+                return true;
+            }
+            set.add(number);
+        }
+        return false;
+    }
+}
