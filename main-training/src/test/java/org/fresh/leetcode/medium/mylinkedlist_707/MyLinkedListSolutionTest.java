4ec49995a81c024253ed7ff275041f66250c84da
Oleksii Pasko <oleksii.pasko@gmail.com>
2021-03-26 19:25:02 +0200

    adding new problem

---
+++ main-training/src/test/java/org/fresh/leetcode/medium/mylinkedlist_707/MyLinkedListSolutionTest.java
@@ -0,0 +1,36 @@
+package org.fresh.leetcode.medium.mylinkedlist_707;
+
+import org.assertj.core.api.Assertions;
+import org.fresh.leetcode.medium.mylinkedlist707.MyLinkedList;
+import org.junit.jupiter.api.Test;
+
+/**
+ * https://leetcode.com/problems/design-linked-list/
+ */
+public class MyLinkedListSolutionTest {
+    @Test
+    public void testMyLinkedList() {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        Assertions.assertThat(myLinkedList.get(0)).isEqualTo(-1);
+        Assertions.assertThat(myLinkedList.get(100)).isEqualTo(-1);
+
+        myLinkedList.addAtHead(1);
+
+        Assertions.assertThat(myLinkedList.get(100)).isEqualTo(-1);
+
+        myLinkedList.addAtTail(3); // linked list becomes 1->3
+        myLinkedList.addAtIndex(1, 2);   // linked list becomes 1->2->3
+
+        myLinkedList.addAtIndex(100, 2); // invalid index. not inserted
+        Assertions.assertThat(myLinkedList.get(0)).isEqualTo(1);
+        Assertions.assertThat(myLinkedList.get(1)).isEqualTo(2);
+        Assertions.assertThat(myLinkedList.get(2)).isEqualTo(3);
+        Assertions.assertThat(myLinkedList.get(2)).isEqualTo(-1);
+        Assertions.assertThat(myLinkedList.get(100)).isEqualTo(-1);
+
+        myLinkedList.deleteAtIndex(1);    // now the linked list is 1->3
+
+        Assertions.assertThat(myLinkedList.get(0)).isEqualTo(1);
+        Assertions.assertThat(myLinkedList.get(1)).isEqualTo(3);
+    }
+}
